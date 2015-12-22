5ddeb36189b6dd49f22935e78c2a145c3db0dd0f
GuoKai <badkaikai@gmail.com>
2015-12-22 21:20:11 +0800

    update files, 全部挂在全局。

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
-
-var paths = {
-    dst: './build/pages/',
-    htmlSrc: ['./src/pages/**/*.html'],
-    jsSrc: ['./src/pages/**/*.js'],
-    cssSrc: ['./src/pages/**/*.scss'],
-    bsSrc: ['./build/pages/**/*']
-};
-
-var source = require('vinyl-source-buffer');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var envify = require('envify');
-var unreachable = require('unreachable-branch-transform');
-var collapser = require('bundle-collapser/plugin');
-var vendors = [
-    'querystring',
-    'vue',
-    'superagent',
-    'fastclick',
-    'cookie-monster',
-    'pinkie-promise'
+var concat = require('gulp-concat');
+var source_files = [
+    './src/cookie-monster/dist/cookie-monster.js',
+    './src/fastclick/lib/fastclick.js',
+    './src/pinkie/index.js',
+    './src/querystring/querystring.js',
+    './src/superagent/superagent.js',
+    './src/vue/dist/vue.js'
 ];
 
-var _bCache = {};
-var _bPackageCache = {};
-
 gulp.task('build', function() {
-    var vendorsBundle = browserify({
-        entries: '',
-        cache: _bCache,
-        packageCache: _bPackageCache
-    });
-
-    vendors.map(function(v) {
-        vendorsBundle.require(v);
-    });
-
-    vendorsBundle = vendorsBundle
-        .transform(babelify.configure({
-            'presets': ['es2015']
-        }))
-        .transform(envify)
-        .transform(unreachable)
-        .plugin(collapser);
-
-    vendorsBundle.bundle()
-        .pipe(source('index.js'))
-        .pipe(gulp.dest('./build'));
-
-    vendorsBundle.on('error', function(err) {
-        console.log('Error: ' + err.message);
-    });
+    return gulp.src(source_files)
+        .pipe(concat('index.js'))
+        .pipe(gulp.dest('./build/'));
 });
